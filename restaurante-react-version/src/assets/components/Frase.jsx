a2a3fb08123533f2baf3d245ae5e529916af6efc
franci5c0 <cursosenaitecnico@gmail.com>
2025-06-26 11:15:30 -0300

    adicionando o react

---
+++ restaurante-react-version/src/assets/components/Frase.jsx
@@ -0,0 +1,9 @@
+function Frase() {
+    return (
+        <div>
+            <p>testando a frase do componente</p>
+        </div>
+    )
+}
+
+export default Frase
---
+++ restaurante-react-version/src/assets/components/HelloWorld.jsx
@@ -0,0 +1,9 @@
+function HelloWorld() {
+    return (
+        <div>
+            <h1>TESTANDO O COMPONENTE</h1>
+        </div>
+    )
+}
+
+export default HelloWorld
